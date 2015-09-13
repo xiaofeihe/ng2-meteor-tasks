d10dd801329c8c925bbc7c2fdf8c4b3c7f97e086
runze.us <110@runze.us>
2015-09-14 00:10:33 +0800

    美化了界面， 完成了点击查看详细信息，以及添加评论功能

--- client/app.ts
+++ client/app.ts
@@ -1,16 +1,14 @@
-
 import {Component, View, bootstrap,NgFor} from 'angular2/angular2';
 
 @Component({
   selector: 'app'
 })
-
 @View({
    templateUrl:'client/tpl/index.ng.html',
   directives:[NgFor]
 })
 class AmsTask {
-tasklist=[]
+tasklist=[],taskDetail={}
   constructor(){
      Tracker.autorun(zone.bind(()=> {
        this.tasklist[0] = Tasks.find({state:0}).fetch();
@@ -18,17 +16,34 @@ tasklist=[]
        this.tasklist[2] = Tasks.find({state:2}).fetch();
     }));
   }
+  //删除任务
   remove(task) {
    Tasks.remove(task._id);
  }
+ //添加任务
  addTask(title,beizu){
-   Tasks.insert({title:title.value,beizu:beizu.value,state:1})
+   Tasks.insert({title:title.value,beizu:beizu.value,state:0,comments:[]})
    title.value=beizu.value=""
  }
+ //更改任务状态
  upState(task,v){
    task.state=v;
    Tasks.update(task._id,task)
  }
+ //点击某个任务后
+ cktask(task){
+      this.taskDetail=task
+      document.getElementById("tkdetail").style.visibility="visible"//visible
+ }
+ //退出详细页面
+ hideDetail(){
+   document.getElementById("tkdetail").style.visibility="hidden"//visible
+ }
+ addComment(task,newjilu){
+   Tasks.update(task._id,{"$push":{"comments":{"content":newjilu.value}}})
+    this.taskDetail['comments'].push({"content":newjilu.value})
+    newjilu.value=""
+    cktask(this.taskDetail)
+ }
 }
-
 bootstrap(AmsTask);
